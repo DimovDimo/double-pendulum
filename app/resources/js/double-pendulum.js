c644dc4b1c1ce31633f54864901a94e3e1f3f1aa
DimovDimo <d.dimov1996@gmail.com>
2020-01-27 19:10:04 +0200

    added index.html & reset-css.css

---
+++ app/resources/js/double-pendulum.js
@@ -0,0 +1,5 @@
+let canvas1 = document.getElementById("canvas-1");
+let canvas2 = document.getElementById("canvas-2");
+
+let context1 = canvas1.getContext("2d");
+let context2 = canvas2.getContext("2d");
