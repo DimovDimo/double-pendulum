38bbe307f96d4bd2651f2dcb449491359fe8252b
DimovDimo <d.dimov1996@gmail.com>
2020-01-31 10:38:57 +0200

    added function pendulumLength

--- app/resources/js/double-pendulum.js
+++ app/resources/js/double-pendulum.js
@@ -1,9 +1,13 @@
 const canvasId1 = "canvas-1";
 const canvasId2 = "canvas-2";
-const gravity = 1;
+const gravity = 1.5;
+const mass1 = 15;
+const mass2 = 7;
+const radius1 = 0.2;
+const radius2 = 0.2;
 
 let canvas1 =  getCanvas(canvasId1);
-let canvas2 =  getCanvas(canvasId2);
+let canvas2 =  getCanvas(canvasId2);//getCanvas(canvasId1);
 
 let context1 = canvas1.getContext("2d");
 let context2 = canvas2.getContext("2d");
@@ -14,15 +18,18 @@ setCanvasWidthAndHeight(canvas2);
 let width = innerWidth;
 let height = innerHeight;
 
+let length1 = pendulumLength(width, height, radius1);
+let length2 = pendulumLength(width, height, radius2);
+
 function getCanvas(canvasId){
     return document.getElementById(canvasId);
-}
-
-function get2dCanvasContext(canvas){
-    return canvas.getContext("2d");
 }
 
 function setCanvasWidthAndHeight(canvas){
     canvas.width = innerWidth;
     canvas.height = innerHeight;
+}
+
+function pendulumLength(width, height, radius){
+    return Math.min(width,height) * radius;
 }
